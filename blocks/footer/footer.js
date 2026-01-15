884b347fefd527ba3dc95aa7c57291b5e32dc7ca
rajnishojha <34776521+rajnishojha@users.noreply.github.com>
2026-01-15 10:36:42 -0500

    Describe your change

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,7 +2,7 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
- * loads and decorates the footer
+ * loads and decorates the footer -rajnish
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
